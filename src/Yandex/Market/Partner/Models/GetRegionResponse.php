007308de457d8b7afb96f258ec824ee170b278cc
Alex <mr@g1k.ru>
2017-03-11 00:02:41 +0300

    Get Region

---
+++ src/Yandex/Market/Partner/Models/GetRegionResponse.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Yandex\Market\Partner\Models;
+
+use Yandex\Common\Model;
+
+class GetRegionResponse extends Model
+{
+    protected $region = null;
+
+    protected $mappingClasses = [
+        'region' => 'Yandex\Market\Partner\Models\Region'
+    ];
+
+    protected $propNameMap = [];
+
+    /**
+     * @return null
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    /**
+     * @param null $region
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+        return $this;
+    }
+}
